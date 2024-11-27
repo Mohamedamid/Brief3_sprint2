ee43314001d46c7a267c2672a8242fb60ce3d217
mohamedamid <amid.mohamed2024@gmail.com>
2024-11-27 17:28:27 +0100

    ajouter le boton delete et edit et travail la fonction delete

--- js/main.js
+++ js/main.js
@@ -106,6 +106,7 @@ function displayPlayerData(playerData) {
 
     if (positionSelect.value === 'GK') {
         playerElement.innerHTML = `
+        <div class="info">
         <img class="photo_joueur" src="${playerData.photo}" alt="" width="50" height="50">
         <strong>${playerData.name}</strong>
         <div class="information_joueur">
@@ -117,9 +118,15 @@ function displayPlayerData(playerData) {
         <p>POS<span> ${playerData.positioning}</span></p>
         </div>
         <strong class="position_joueur">${playerData.position}</strong>
+        </div>
+        <div class="action">
+        <i class="fa-solid fa-trash" onclick="deleteT(event)"></i>
+        <i class="fa-solid fa-pen-to-square" onclick="edit_GK(event)"></i>
+        </div>
     `;
     } else {
         playerElement.innerHTML = `
+        <div class="info">
         <img class="photo_joueur" src="${playerData.photo}" alt="" width="50" height="50">
         <strong>${playerData.name}</strong>
         <div class="information_joueur">
@@ -131,6 +138,11 @@ function displayPlayerData(playerData) {
         <p>PHY<span> ${playerData.physical}</span></p>
         </div>
         <strong class="position_joueur">${playerData.position}</strong>
+        </div>
+        <div class="action">
+        <i class="fa-solid fa-trash" onclick="deleteT(event)"></i>
+        <i class="fa-solid fa-pen-to-square" onclick="edit_joueur(event)"></i>
+        </div>
     `;
     }
 
@@ -179,104 +191,136 @@ function addPlayerToReserve(playerData) {
     playerElement.style.backgroundRepeat = 'no-repeat';
 
     if (positionSelect.value === 'GK') {
-        playerElement.innerHTML = `
-        <img class="photo_joueur" src="${playerData.photo}" alt="" width="50" height="50">
-        <strong>${playerData.name}</strong>
-        <div class="information_joueur">
-        <p>DIV<span> ${playerData.diving}</span></p>
-        <p>HAN<span> ${playerData.handling}</span></p>
-        <p>KIC<span> ${playerData.kicking}</span></p>
-        <p>REF<span> ${playerData.reflexes}</span></p>
-        <p>SPE<span> ${playerData.speed}</span></p>
-        <p>POS<span> ${playerData.positioning}</span></p>
-        </div>
-        <strong class="position_joueur">${playerData.position}</strong>
-    `;
-    } else {
-        playerElement.innerHTML = `
-        <img class="photo_joueur" src="${playerData.photo}" alt="" width="50" height="50">
-        <strong>${playerData.name}</strong>
-        <div class="information_joueur">
-        <p>PAC<span> ${playerData.pace}</span></p>
-        <p>SHO<span> ${playerData.shooting}</span></p>
-        <p>PAS<span> ${playerData.passing}</span></p>
-        <p>DRI<span> ${playerData.dribbling}</span></p>
-        <p>DEF<span> ${playerData.defending}</span></p>
-        <p>PHY<span> ${playerData.physical}</span></p>
-        </div>
-        <strong class="position_joueur">${playerData.position}</strong>
-    `;
-    }
-
+            playerElement.innerHTML = `
+            <div class="info">
+            <img class="photo_joueur" src="${playerData.photo}" alt="" width="50" height="50">
+            <strong>${playerData.name}</strong>
+            <div class="information_joueur">
+            <p>DIV<span> ${playerData.diving}</span></p>
+            <p>HAN<span> ${playerData.handling}</span></p>
+            <p>KIC<span> ${playerData.kicking}</span></p>
+            <p>REF<span> ${playerData.reflexes}</span></p>
+            <p>SPE<span> ${playerData.speed}</span></p>
+            <p>POS<span> ${playerData.positioning}</span></p>
+            </div>
+            <strong class="position_joueur">${playerData.position}</strong>
+            </div>
+            <div class="action">
+            <i class="fa-solid fa-trash" onclick="delete_joueur_reserve(event)"></i>
+            <i class="fa-solid fa-pen-to-square" onclick="edit_GK(event)"></i>
+            </div>
+        `;
+        } else {
+            playerElement.innerHTML = `
+            <div class="info">
+            <img class="photo_joueur" src="${playerData.photo}" alt="" width="50" height="50">
+            <strong>${playerData.name}</strong>
+            <div class="information_joueur">
+            <p>PAC<span> ${playerData.pace}</span></p>
+            <p>SHO<span> ${playerData.shooting}</span></p>
+            <p>PAS<span> ${playerData.passing}</span></p>
+            <p>DRI<span> ${playerData.dribbling}</span></p>
+            <p>DEF<span> ${playerData.defending}</span></p>
+            <p>PHY<span> ${playerData.physical}</span></p>
+            </div>
+            <strong class="position_joueur">${playerData.position}</strong>
+            </div>
+            <div class="action">
+            <i class="fa-solid fa-trash" onclick="delete_joueur_reserve(event)"></i>
+            <i class="fa-solid fa-pen-to-square" onclick="edit_joueur(event)"></i>
+            </div>`
+            ;
+        }
     reserveDiv.appendChild(playerElement);
 }
 
-function modifier(event) {
-    const playerElement = event.target.closest('.player-item');
+function deleteT(event) {
+    let playerElement = event.currentTarget.closest(".player-item");
 
-    const playerNameElement = playerElement.querySelector('strong');
-    const playerPhotoElement = playerElement.querySelector('.photo_joueur');
-    const positionElement = playerElement.querySelector('.position_joueur');
+    const position = playerElement.querySelector(".position_joueur").textContent;
     
-    const nationality = playerElement.querySelector('.information_joueur p:nth-child(1) span')?.textContent || '';
-    const flag = playerElement.querySelector('.flag')?.src || ''; // العلم
-    const club = playerElement.querySelector('.club')?.textContent || ''; // النادي
-    const logo = playerElement.querySelector('.logo')?.src || ''; // الشعار
-    const rating = playerElement.querySelector('.rating')?.textContent || ''; // التقييم
-
-    const playerName = playerNameElement ? playerNameElement.textContent : '';
-    const playerPhoto = playerPhotoElement ? playerPhotoElement.src : '';
-    const position = positionElement ? positionElement.textContent : '';
-
-    let pace = playerElement.querySelector('.pace')?.textContent || '';
-    let shooting = playerElement.querySelector('.shooting')?.textContent || '';
-    let passing = playerElement.querySelector('.passing')?.textContent || '';
-    let dribbling = playerElement.querySelector('.dribbling')?.textContent || '';
-    let defending = playerElement.querySelector('.defending')?.textContent || '';
-    let physical = playerElement.querySelector('.physical')?.textContent || '';
-
-    let diving = playerElement.querySelector('.diving')?.textContent || '';
-    let handling = playerElement.querySelector('.handling')?.textContent || '';
-    let kicking = playerElement.querySelector('.kicking')?.textContent || '';
-    let reflexes = playerElement.querySelector('.reflexes')?.textContent || '';
-    let speed = playerElement.querySelector('.speed')?.textContent || '';
-    let positioning = playerElement.querySelector('.positioning')?.textContent || '';
-
-    // ملء الحقول في النموذج
-    document.getElementById('name').value = playerName;
-    document.getElementById('photo').value = playerPhoto;
-    document.getElementById('position').value = position;
-    document.getElementById('Nationality').value = nationality;
-    document.getElementById('flag').value = flag;
-    document.getElementById('club').value = club;
-    document.getElementById('logo').value = logo;
-    document.getElementById('rating').value = rating;
-
-    // ملء البيانات الخاصة بالمحترفين
-    document.getElementById('pace').value = pace;
-    document.getElementById('shooting').value = shooting;
-    document.getElementById('passing').value = passing;
-    document.getElementById('dribbling').value = dribbling;
-    document.getElementById('defending').value = defending;
-    document.getElementById('physical').value = physical;
-
-    // ملء البيانات الخاصة بحراس المرمى (إن كانت موجودة)
-    document.getElementById('diving').value = diving;
-    document.getElementById('handling').value = handling;
-    document.getElementById('kicking').value = kicking;
-    document.getElementById('reflexes').value = reflexes;
-    document.getElementById('speed').value = speed;
-    document.getElementById('positioning').value = positioning;
-
-    // تغيير العرض بين النموذج العادي وحارس المرمى
-    if (position === 'GK') {
-        // عرض الحقول الخاصة بحارس المرمى
-        document.querySelector('.gardiyan').style.display = 'flex';
-        document.querySelector('.joueur').style.display = 'none';
-    } else {
-        // عرض الحقول الخاصة باللاعبين العاديين
-        document.querySelector('.gardiyan').style.display = 'none';
-        document.querySelector('.joueur').style.display = 'flex';
+    if (position) {
+        if (position === 'CF') {
+            alert(`You can add another player in the ${position} position.`);
+            playerCounts['Attaque']--; 
+        } else if (position === 'CM') {
+            alert(`You can add another player in the ${position} position.`);
+            playerCounts['milieu']--;
+        } else if (position === 'CB') {
+            alert(`You can add another player in the ${position} position.`);
+            playerCounts['Defense']--;
+        } else if (position === 'GK') {
+            alert(`You can add another player in the ${position} position.`);
+            playerCounts['garde']--;
+        }
     }
+    playerElement.remove();
+
+}
+function delete_joueur_reserve(event) {
+    let playerElement = event.currentTarget.closest(".player-item");
+    playerElement.remove();
 }
 
+function edit_GK(event) {
+    alert('ggggggggggggg')
+
+    currentRow = event.currentTarget.closest("tr");
+    document.getElementById("name").value = currentRow.cells[0].textContent;
+    document.getElementById("photo").value = currentRow.cells[1].textContent;
+    document.getElementById("position").value = currentRow.cells[2].textContent;
+    document.getElementById("Nationality").value = currentRow.cells[3].textContent;
+    document.getElementById("flag").value = currentRow.cells[1].textContent;
+    document.getElementById("club").value = currentRow.cells[2].textContent;
+    document.getElementById("logo").value = currentRow.cells[3].textContent;
+    document.getElementById("rating").value = currentRow.cells[1].textContent;
+    document.getElementById("pace").value = currentRow.cells[2].textContent;
+    document.getElementById("Tir").value = currentRow.cells[3].textContent;
+    document.getElementById("passing").value = currentRow.cells[1].textContent;
+    document.getElementById("dribbling").value = currentRow.cells[2].textContent;
+    document.getElementById("defending").value = currentRow.cells[3].textContent;
+    document.getElementById("physical").value = currentRow.cells[1].textContent;
+    document.getElementById("diving").value = currentRow.cells[2].textContent;
+    document.getElementById("handling").value = currentRow.cells[3].textContent;
+    document.getElementById("kicking").value = currentRow.cells[3].textContent;
+    document.getElementById("reflexes").value = currentRow.cells[1].textContent;
+    document.getElementById("speed").value = currentRow.cells[2].textContent;
+    document.getElementById("positioning").value = currentRow.cells[3].textContent;
+
+    document.getElementById("club").style.display = "none";
+    document.getElementById("editButton").style.display = "inline-block";
+}
+
+function save_GK() {
+    currentRow.cells[0].textContent = document.getElementById("title").value;
+    currentRow.cells[1].textContent = document.getElementById("description").value;
+    currentRow.cells[2].textContent = document.getElementById("date").value;
+    currentRow.cells[3].textContent = document.getElementById("options").value;
+    document.getElementById("addtaskform").reset();
+    document.getElementById("editButton").style.display = "none";
+    document.getElementById("ajouter").style.display = "flex";
+    currentRow = null;
+}
+
+function edit_joueur(event) {
+    alert('hhhhhhhhhh')
+    currentRow = event.currentTarget.closest("tr");
+    document.getElementById("title").value = currentRow.cells[0].textContent;
+    document.getElementById("description").value = currentRow.cells[1].textContent;
+    document.getElementById("date").value = currentRow.cells[2].textContent;
+    document.getElementById("options").value = currentRow.cells[3].textContent;
+    document.getElementById("ajouter").style.display = "none";
+    document.getElementById("editButton").style.display = "inline-block";
+}
+
+function save_joueur() {
+    if (!currentRow) return;
+    currentRow.cells[0].textContent = document.getElementById("title").value;
+    currentRow.cells[1].textContent = document.getElementById("description").value;
+    currentRow.cells[2].textContent = document.getElementById("date").value;
+    currentRow.cells[3].textContent = document.getElementById("options").value;
+    document.getElementById("addtaskform").reset();
+    document.getElementById("editButton").style.display = "none";
+    document.getElementById("ajouter").style.display = "flex";
+    currentRow = null;
+}
